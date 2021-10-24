c46ee2a174af717e012d7b6840c9a45022deef79
Marjorhi <marjoriejetajobe0524@gmail.com>
2021-10-25 04:54:59 +0800

    Disabling the send button to app.js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// Variables
+const sendBtn = document.getElementById('sendBtn');
+
+
+
+// Event Listeners
+
+eventListeners();
+
+function eventListeners () {
+    // App Init
+    document.addEventListener('DOMContentLoaded', appInit);
+}
+
+
+// Functions
+
+//App Initialization
+function appInit () {
+    // Disable  the send button on load
+    sendBtn.disabled = true;
+}
