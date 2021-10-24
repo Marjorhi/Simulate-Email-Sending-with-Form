6e6f9edd5afa8b08c02e8286dbfde026f897f2ed
Marjorhi <marjoriejetajobe0524@gmail.com>
2021-10-25 05:40:01 +0800

    Validating if the emailText contains the @ sign to app.js

--- js/app.js
+++ js/app.js
@@ -34,11 +34,29 @@ function validateField() {
     let errors;
 
     // Validate the length of the Field
-    validateLength(this)
+    validateLength(this);
+
+    // Validate the email
+    if(this.type === 'email') {
+        validateEmail(this);
+    }
 }
 //Validate the Length of the Fields
 function validateLength(field) {
     if(field.value.length > 0) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
+}
+// Validate email (checks for @ in the value)
+
+function validateEmail(field) {
+    let emailText = field.value;
+    //check if the emailText contains the @ sign
+    if(emailText.indexOf('@') !== -1) {
         field.style.borderBottomColor = 'green';
         field.classList.remove('error');
     } else {
